3b7dd190307fd8a311300160b2ec61f9adb5dc81
c_laigle_catie <c.laigle@catie.fr>
2021-10-21 16:53:33 +0200

    Add start, stop, set_gain functions

--- src/ads131a04.cpp
+++ src/ads131a04.cpp
@@ -6,59 +6,36 @@
 
 namespace sixtron {
 
-ADS131A04::ADS131A04(SPI *spi, PinName cs):_spi(spi)
+ADS131A04::ADS131A04(SPI *spi, PinName cs, PinName reset): _spi(spi)
 {
     _cs = new DigitalOut(cs);
+
+    _reset = new DigitalOut(reset);
+
+    _spi->format(8, 1);
 }
 
 void ADS131A04::reset()
 {
+    // TODO : Implement
 }
 
 uint8_t ADS131A04::init()
 {
-    reset();
-
-    *_cs = 1;
-
-
-    uint8_t reg = 0;
-
-    spi_read_register(RegisterAddress::id_lsb, &reg);
-
-    if (reg != CHIP_ID) {
-        ThisThread::sleep_for(1s);
-        spi_read_register(RegisterAddress::id_lsb, &reg);
-        if (reg != CHIP_ID) {
-            return -1;
-        }
-    }
-
     uint16_t status = 0;
-    
+
     send_command(Command::null, &status);
 
-    if(status == 0xff04)
-    {
+    if (status == 0xff04) {
         status = send_command(Command::unlock);
 
-        if(status == 0x0655)
-        {
+        if (status == 0x0655) {
             spi_write_register(RegisterAddress::clk1, 0x02); // CLKIN/2
 
             spi_write_register(RegisterAddress::clk2, 0x25); // FMOD/512 | ICLK/2
 
-            // Set GAIN=2 for all ADCs
-            spi_write_register(RegisterAddress::adc1, 0x01); // TODO Move this on a function
-            spi_write_register(RegisterAddress::adc2, 0x01); // TODO Move this on a function
-            spi_write_register(RegisterAddress::adc3, 0x01); // TODO Move this on a function
-            spi_write_register(RegisterAddress::adc4, 0x01); // TODO Move this on a function
-
             // Enable All ADC channel
             spi_write_register(RegisterAddress::adc_ena, 0x0F);
-
-            // Wake-up from standby mode
-            send_command(Command::wakeup);
 
             send_command(Command::lock);
         }
@@ -67,18 +44,84 @@ uint8_t ADS131A04::init()
     return 0;
 }
 
+uint8_t ADS131A04::start()
+{
+    uint16_t status = 0;
+
+    status = send_command(Command::unlock);
+
+    if (status == 0x0655) {
+        // Enable All ADC channel
+        spi_write_register(RegisterAddress::adc_ena, 0x0F);
+
+        // Wake-up from standby mode
+        send_command(Command::wakeup);
+
+        send_command(Command::lock);
+
+        return 0;
+    }
+
+    return -1;
+}
+
+uint8_t ADS131A04::stop()
+{
+    uint16_t status = 0;
+
+    status = send_command(Command::unlock);
+
+    if (status == 0x0655) {
+        // Enable All ADC channel
+        spi_write_register(RegisterAddress::adc_ena, 0x00);
+
+        // Wake-up from standby mode
+        send_command(Command::wakeup);
+
+        send_command(Command::lock);
+
+        return 0;
+    }
+
+    return -1;
+}
+
 uint8_t ADS131A04::set_gain(ADC adc, uint8_t gain)
 {
+    switch (adc) {
+        case ADC::adc1:
+            spi_write_register(RegisterAddress::adc1, gain);
+            break;
+        case ADC::adc2:
+            spi_write_register(RegisterAddress::adc2, gain);
+            break;
+        case ADC::adc3:
+            spi_write_register(RegisterAddress::adc3, gain);
+            break;
+        case ADC::adc4:
+            spi_write_register(RegisterAddress::adc4, gain);
+            break;
+        case ADC::all:
+            spi_write_register(RegisterAddress::adc1, gain);
+            spi_write_register(RegisterAddress::adc2, gain);
+            spi_write_register(RegisterAddress::adc3, gain);
+            spi_write_register(RegisterAddress::adc4, gain);
+            break;
+        default:
+            return -1;
+    }
+
     return 0;
 }
 
 uint8_t ADS131A04::spi_read_register(RegisterAddress registerAddress, uint8_t *value)
 {
-    uint16_t word = static_cast<uint8_t>(Command::rreg) | (static_cast<uint8_t>(registerAddress) << 8);
+    uint16_t word
+            = static_cast<uint8_t>(Command::rreg) | (static_cast<uint8_t>(registerAddress) << 8);
 
-    char txData[2] = {0};
+    char txData[2] = { 0 };
     txData[1] = (word & 0xff);
-	txData[0] = (word >> 8);
+    txData[0] = (word >> 8);
 
     char ret[3]; // = 0;
 
@@ -88,7 +131,7 @@ uint8_t ADS131A04::spi_read_register(RegisterAddress registerAddress, uint8_t *v
 
     *_cs = 1;
 
-    for(int i =0; i<3; i++){
+    for (int i = 0; i < 3; i++) {
         printf("%d\n", ret[i]);
     }
 
@@ -99,11 +142,12 @@ uint8_t ADS131A04::spi_read_register(RegisterAddress registerAddress, uint8_t *v
 
 uint8_t ADS131A04::spi_write_register(RegisterAddress registerAddress, uint8_t value)
 {
-    uint16_t word = static_cast<uint8_t>(Command::wreg) | static_cast<uint8_t>(registerAddress) >> 8;
+    uint16_t word
+            = static_cast<uint8_t>(Command::wreg) | static_cast<uint8_t>(registerAddress) >> 8;
 
     *_cs = 0;
 
-    _spi->write((const char*)&word, 2, nullptr, 0);
+    _spi->write((const char *)&word, 2, nullptr, 0);
 
     *_cs = 1;
 
@@ -116,7 +160,7 @@ uint8_t ADS131A04::send_command(Command command, uint16_t *value)
 
     *_cs = 0;
 
-    _spi->write((const char*)&word, 2, (char*)value, 2);
+    _spi->write((const char *)&word, 2, (char *)value, 2);
 
     *_cs = 1;
 
@@ -124,4 +168,3 @@ uint8_t ADS131A04::send_command(Command command, uint16_t *value)
 }
 
 } // namespace sixtron
-
